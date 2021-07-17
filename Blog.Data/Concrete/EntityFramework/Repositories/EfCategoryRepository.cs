4294c6c5cbedf1faf27664dd668d0c4fd092fd28
fgulpolat <49431870+fgulpolat@users.noreply.github.com>
2021-07-17 13:16:30 +0300

    Repository classes added.

---
+++ Blog.Data/Concrete/EntityFramework/Repositories/EfCategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Data.Abstract;
+using Blog.Entities.Concrete;
+using Blog.Shared.Data.Concrete.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Data.Concrete.EntityFramework.Contexts
+{
+    public class EfCategoryRepository : EFEntityRepository<Category>, ICategoryRepository
+    {
+        public EfCategoryRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
